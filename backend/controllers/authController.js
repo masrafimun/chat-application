f3cd325dfa368f59abd3be54b84d5d611afbf362
masrafimun <masrafimunna59@gmail.com>
2025-04-20 22:37:21 +0600

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -164,7 +164,7 @@ export  const veritfyOtp = async (req,res) =>{
         res.cookie('token',token, {
             httpOnly : true,
             secure : true,
-            sameSite : 'none',
+            sameSite : 'None',
             maxAge : 30 * 24 * 60 * 60 * 1000,
             path: "/",
         })
