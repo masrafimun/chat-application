9ff16d8e67271978bbc1685a36b076c5b283901d
masrafimun <masrafimunna59@gmail.com>
2025-04-20 22:31:53 +0600

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -287,8 +287,8 @@ export const logout = async (req,res) =>{
     try{
           res.clearCookie('token',{
             httpOnly : true,
-            secure : process.env.NODE_ENV === 'production',
-            sameSite : process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            secure : true,
+            sameSite : none,
             path: "/",
         })
 
