108332e9703c9491280fa91d2d6310e7a31a3e47
masrafimun <masrafimunna59@gmail.com>
2025-04-21 01:51:31 +0600

    Update People.jsx

--- frontend/component/People.jsx
+++ frontend/component/People.jsx
@@ -62,15 +62,15 @@ const People = ({name,email,profilePic,username,receiverId}) => {
 
     
     return (
-        <div className='flex items-center justify-between border sm:px-7 sm:py-1 mb-6 '>
+     <div className='flex items-center border sm:px-7 sm:py-1 mb-4 '>
             <div className='mr-3 '>
-                <img className='h-[120px] w-[105px] sm:border border-r' src={profilePic || profile_image} alt="" />
+                <img className='h-[125px] w-[150px] sm:border border-r' src={profilePic || profile_image} alt="" />
             </div>
-            <div className='flex flex-col gap-1 sm:gap-2  sm:text-[20px]'>
+            <div className='flex mr-3 w-full  flex-col gap-1 sm:gap-2  sm:text-[20px]'>
                 <p>Name : {name} </p>
-                <p className=''>Username :{username} </p>
-                <p>Email :{email} </p>
-                <p onClick={status === 'Add' ? sendreq :  (status === 'Requested' ? cancelSendReq : null)} className='sm:hidden  bg-cyan-700 text-white text-center rounded-md'>{status}</p>
+                <p className='mb-2 mt-1'>Username :{username} </p>
+                {/* <p>Email :{email} </p> */}
+                <p onClick={status === 'Add' ? sendreq :  (status === 'Requested' ? cancelSendReq : null)} className='sm:hidden mb-2  bg-cyan-700 text-white text-center rounded-md'>{status}</p>
             </div>
             <div>
                 <p onClick={status === 'Add' ? sendreq :  (status === 'Requested' ? cancelSendReq : null)}  className='hidden sm:block bg-cyan-700 text-2xl px-7 py-1 text-white text-center rounded-md'>{status}</p>
