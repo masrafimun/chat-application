79c983c7fd16b23ddb995d065524433c590327fd
masrafimun <masrafimunna59@gmail.com>
2025-04-21 02:23:00 +0600

    Update Friends.jsx

--- frontend/component/Friends.jsx
+++ frontend/component/Friends.jsx
@@ -59,28 +59,28 @@ const Friends = () => {
 
     return (
         
-          <div className='px-4 pt-[60px] sm:pt-0  my-8'>
-            <p className=' text-3xl mb-4'>Friends : </p>
+          <div className='px-4 w-full pt-[28px] sm:pt-0  my-8'>
+            <p className=' text-2xl mb-2'>Friends : </p>
 
             {
             friendsList.length != 0 ? 
               (friendsList.map((user,index) => 
-                    <div key={index} className='flex items-center bg-blue-200 justify-between border mb-6 '>
-                        <div className='mr-3 bg-amber-400'>
+                    <div key={index} className='flex w-full items-center bg-blue-200 border mb-6 '>
+                        <div className='mr-3 bg-amber-400 w-[25%]'>
                             <img className='h-[120px] w-[105px]  border-r' src={user.profilePhoto || profile_image} alt="" />
                         </div>
-                        <div className='flex flex-col gap-1 pr-2 sm:pr-20'>
-                            <p>Name : {user.name} </p>
-                            <p className=''>Username :{user.userName} </p>
+                       
+                        <div className='flex w-[80%] flex-col gap-1 pr-2 sm:pr-20'>
+                            <p className='text-[16px]'>{user.name}</p>
+                            {/* <p className=''>Username :{user.userName} </p> */}
                             <p>Email :{user.email} </p>
 
                             <div className=' flex'>
                                <p onClick={() =>Unfriend(userId,user._id)}  className='px-4 bg-[#24bab8] text rounded-md cursor-pointer mr-4'>Unfriend</p>
-                               <p   className={`px-4 ${isOnline(user._id) ? 'bg-green-500' : 'bg-gray-400'} text rounded-md cursor-pointer mr-4`}>{isOnline(user._id) ? 'Active' : 'Offline'}</p>
-                               <Link to={`/message/${user._id}`}  className='px-4 bg-[#24bab8] text rounded-md cursor-pointer mr-4'>Messsage</Link>
+                            <Link to={`/message/${user._id}`}  className='px-4  bg-[#24bab8]  rounded-md  cursor-pointer mr-4'>Messsage</Link>
                             </div>
+                            <p   className={`px-4 w-[40%] ${isOnline(user._id) ? 'bg-green-500' : 'bg-gray-400'} text rounded-md cursor-pointer mr-4`}>{isOnline(user._id) ? 'Active' : 'Offline'}</p>
                         </div>
-
                         <div>
                         </div>
                     </div>
