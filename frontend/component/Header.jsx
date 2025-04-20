7bb16864863c68ceb7a1d6638685a79903baaf27
masrafimun <masrafimunna59@gmail.com>
2025-04-21 01:03:11 +0600

    Update Header.jsx

--- frontend/component/Header.jsx
+++ frontend/component/Header.jsx
@@ -6,10 +6,10 @@ const Header = () => {
 
     const navigate = useNavigate()
     return (
-        <div className='flex flex-col py-7  items-center text-center  mt-30 sm:mt-40'>
-            <img className=' h-[200px]' src={chatbot} alt="" />
-            <h1 className='text-4xl font-medium mb-7 text-[#24bab8]'>Welcome to our chat app</h1>
-            <button onClick={()=>navigate('/login')} className='font-medium py-3 text-white border-2 text-2xl  rounded-full px-12 bg-[#24bab8]'>Get Started</button>
+        <div className='flex flex-col py-7  items-center text-center  mt-16  sm:mt-40'>
+            <img className=' h-[170px]' src={chatbot} alt="" />
+            <h1 className='text-3xl sm:text-4xl font-medium mb-7 text-[#24bab8]'>Welcome to our chat app</h1>
+            <button onClick={()=>navigate('/login')} className='font-medium py-2 sm:py-3 text-white border-2 text-2xl  rounded-full px-10 sm:px-12 bg-[#24bab8]'>Get Started</button>
         </div>
     );
 };
