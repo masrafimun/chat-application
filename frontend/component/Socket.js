81b88052ef39cb95a14b815439717ace91a863fc
masrafimun <masrafimunna59@gmail.com>
2025-04-20 18:25:59 +0600

    Update Socket.js

--- frontend/component/Socket.js
+++ frontend/component/Socket.js
@@ -3,7 +3,7 @@ import { io } from 'socket.io-client';
   const socket = io("https://chat-backend-vbqg.onrender.com", {
     withCredentials: true,
     autoConnect: false,
-    transports: ['websocket', 'polling']
+    transports: ['websocket']
   });
 
 export default socket;
