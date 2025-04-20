085c9f2f05dc9816594e67676448812603984f2e
masrafimun <masrafimunna59@gmail.com>
2025-04-20 13:24:45 +0600

    Update Socket.js

--- frontend/component/Socket.js
+++ frontend/component/Socket.js
@@ -3,7 +3,7 @@ import { io } from 'socket.io-client';
   const socket = io("https://chat-backend-seven-pi.vercel.app", {
     withCredentials: true,
     autoConnect: false,
-  
+    transports: ['websocket', 'polling']
   });
 
 export default socket;
