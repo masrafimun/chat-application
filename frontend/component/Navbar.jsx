c5872fd6fddb93c4bbc78e5dd9651d2ac8ff4745
masrafimun <masrafimunna59@gmail.com>
2025-04-21 01:02:19 +0600

    Update Navbar.jsx

--- frontend/component/Navbar.jsx
+++ frontend/component/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     const navigate = useNavigate()
 
     return (
-        <div className='bg-[#24bab8]  w-full sm:hidden  flex justify-center sm:justify-between items-center px-2 sm:px-8 py-4'>
+         <div className='bg-[#24bab8]  w-full sm:hidden  flex justify-center sm:justify-between items-center px-2 sm:px-8 py-1'>
             <h1 className='text-3xl py-1 sm:py-0 text-white font-medium '>CHATAPP</h1>
             <button onClick={() => navigate('/login')} className='text-2xl hidden sm:block  border-2 text-black border-black  bg-gray-100 px-6 py-2 rounded-full hover:bg-white' >Login</button>
         </div>
